445c997c76a18e30c9e99a2fab16434d6a1c901b
Jong Hoon Lee <whdgns0665@Jongs-Air.fios-router.home>
2018-12-27 23:43:32 -0500

    adding REST apis

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'social_django' # <- Here
+    'social_django',
+    'api'
 
 ]
 
--- django_auth/urls.py
+++ django_auth/urls.py
@@ -3,8 +3,12 @@ from django.contrib import admin
 from django.contrib.auth import views
 from .views import home
 
+from api.resources import NoteResource
+note_resource = NoteResource()
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^api/', include(note_resource.urls)),
     url(r'^login/$', views.LoginView.as_view(), name='login'),
     url(r'^logout/$', views.LogoutView.as_view(), name='logout'),
     url(r'^auth/', include('social_django.urls', namespace='social')),  # <- Here
